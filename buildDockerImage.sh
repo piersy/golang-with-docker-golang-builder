a0d08872ccdd15f0d6f5a50a27fe7676bf0b34ce
piersy <pierspowlesland@gmail.com>
2015-04-26 22:36:22 +0100

    updated script for readability

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-DOCKER_GOLANG_BUILDER="piersy/docker-golang-builder:1.0";
-IMAGE_NAME="golang_with_docker";
-BUILD_DIR="/build";
 
-echo "Pulling ${DOCKER_GOLANG_BUILDER}"
-sudo docker pull "${DOCKER_GOLANG_BUILDER}";
+CURRENT_DIR="$(pwd)";
+#remove longest match of "*/" from front of CURRENT_DIR
+DIRNAME=${CURRENT_DIR##*/}
 
-echo "Building ${IMAGE_NAME}"
-sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -v $(pwd):"${BUILD_DIR}" -t "${DOCKER_GOLANG_BUILDER}" bash -c "(cd ${BUILD_DIR} && go build) && sudo docker build -t ${IMAGE_NAME} /build"
+echo "Pulling docker-golang-builder"
+sudo docker pull "piersy/docker-golang-builder:1.0";
+echo "Building golang_with_docker"
+sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -v "${CURRENT_DIR}:/${DIRNAME}" -t "piersy/docker-golang-builder:1.0" bash -c "(cd ${DIRNAME} && go build ) && sudo docker build -t golang_with_docker /${DIRNAME}"
 
 
